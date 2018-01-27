2cafea963cb2178a8a3f819c0d9d46dcd61458c6
AngeliSoul <825661121@qq.com>
2018-01-27 16:39:56 +0800

    Add python
    First upload

---
+++ notebook.py
@@ -0,0 +1,21 @@
+age = 24
+name = "Atlas"
+
+print("{} was {} years old when he write this book.".format(name,age))
+print("Why is {} playing with that python?".format(name))
+ 
+#
+
+print("{0:.3f}".format(1.0/3))
+print("{0:_^11}".format("Hello"))
+print("{name} wrote {book}".format(name="Atlas",book="A byte of python."))
+ 
+print("a",end="")
+print("b",end="")
+
+
+print("a")
+print("b")
+
+
+print("c",end="")
